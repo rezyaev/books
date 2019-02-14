4335654581ecb82dfb482b76c0e5b6bd5d36b574
Danil Rezyayev <danil.rezyayev@gmail.com>
2019-02-14 21:38:09 +0500

    Create import script

---
+++ import.py
@@ -0,0 +1,20 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    f = open("books.csv")
+    reader = csv.reader(f)
+    for isbn, title, author, year in reader:
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                    {"isbn": isbn, "title": title, "author": author, "year": year})
+        print(f"Added book {title}, {author}, {year} with ISBN = {isbn}")
+    db.commit()
+
+if __name__ == "__main__":
+    main()
